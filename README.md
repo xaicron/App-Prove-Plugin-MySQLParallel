b201899a50ddf934cd48d040412cc2a7071fbcef
xaicron <xaicron@gmail.com>
2013-05-06 18:03:06 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Prove::Plugin::MySQLParallel - It's new $module
+
+# SYNOPSIS
+
+    use App::Prove::Plugin::MySQLParallel;
+
+# DESCRIPTION
+
+App::Prove::Plugin::MySQLParallel is ...
+
+# LICENSE
+
+Copyright (C) xaicron.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+xaicron <xaicron@gmail.com>
---
+++ lib/App/Prove/Plugin/MySQLParallel.pm
@@ -0,0 +1,39 @@
+package App::Prove::Plugin::MySQLParallel;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Prove::Plugin::MySQLParallel - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Prove::Plugin::MySQLParallel;
+
+=head1 DESCRIPTION
+
+App::Prove::Plugin::MySQLParallel is ...
+
+=head1 LICENSE
+
+Copyright (C) xaicron.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@gmail.comE<gt>
+
+=cut
+
